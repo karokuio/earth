46dc0fbc6f0f88370b2c91d1b28b21919612dfdf
Mario Garcia <mario.ggar@gmail.com>
2017-07-13 22:15:05 +0200

    add travis integration

--- docker/files/dev-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-tmux -2 new-session -d -s earth
-tmux rename-window -t earth:0 'dev'
-tmux select-window -t earth:0
-tmux send-keys -t earth:0 'cat welcome' C-m
-
-tmux select-window -t earth:0
-tmux -2 attach-session -t earth
